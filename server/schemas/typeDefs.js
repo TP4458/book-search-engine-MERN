d7a03e3f5e561001a7521435c976b57ed65acfe9
Tomasz Pawlikowski <tp4458@gmail.com>
2024-03-04 00:23:53 +0000

    typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    findUser: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  } 
+
+  input bookData {}
+    bookId: ID!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type mutation {
+    login(email: String!, password: String!): Auth!
+    newUser(username: String!, email: String!, password: String!): Auth
+    saveBook( input: bookData! ): User
+    delBook(bookId:ID!): User
+  }
+`;
+
+module.exports = typeDefs;
+// creating an object in a database and updating an object in a database often take the same parameters.
+// To make your schema simpler, you can use “input types” for this,
+// by using the input keyword instead of the type keyword.
